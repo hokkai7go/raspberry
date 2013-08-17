9f1e9f9e757dc6b3f01149dc824e6ff9fc09def3
Yutaro Sugai <y.sugai@everyleaf.com>
2013-08-17 18:06:44 +0900

    Specify log path

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :output, '/home/pi/log'
+set :output, {:error => '/home/pi/log/error.log', :standard => '/home/pi/log/standard.log' }
 
 every 1.hours do
   command "/usr/bin/bundle exec /usr/bin/ruby raspberry_ip_checker.rb | xargs /usr/bin/bundle exec /usr/bin/ruby pircbot.rb"
