83527a5213f1fab64b84dcdbdca8d7ffa0a06a0f
Yutaro Sugai <y.sugai@everyleaf.com>
2013-08-18 00:30:52 +0900

    Specify directory of source code

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, {:error => '/home/pi/log/error.log', :standard => '/home/pi/log/standard.log' }
 
 every 1.hours do
-  command "/usr/bin/bundle exec /usr/bin/ruby raspberry_ip_checker.rb | xargs /usr/bin/bundle exec /usr/bin/ruby pircbot.rb"
+  command "cd /home/pi/src/raspberry && /usr/bin/bundle exec /usr/bin/ruby raspberry_ip_checker.rb | xargs /usr/bin/bundle exec /usr/bin/ruby pircbot.rb"
 end
